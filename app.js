48cf99e81b51d035cd179d02bc00f40ccd868011
Yub32484 <45743401+Yub32484@users.noreply.github.com>
2020-05-07 23:13:01 -0500

    2nd day1 commit

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var port = process.env.PORT || 4000;
 var exp = require("express");
 var app = exp();
-app.use(exp.static(__dirname + "/public"));
+app.get('/players', function(req, res) {
+    var query = req.query;
+    console.log("query valur" + JSON.stringify(query));
+    res.write("name=" + query.name);
+    res.end(" lang=" + query.lang);
+    res.end(JSON.stringify(query));
+});
+//app.use(exp.static(__dirname + "/public"));
 //along with this create public folder and create one Index.html page and try to access port in web page 
+var port = process.env.PORT || 5555;
 app.listen(port, function(params) {
     console.log("Server is listing on port" + port);
 });
